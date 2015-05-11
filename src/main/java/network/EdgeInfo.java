84a275815f9718b0ea53e85499c646f71836d03e
nweill <nweill@viascience.com>
2015-05-11 10:05:46 -0400

    edgeInfo

--- src/main/java/network/EdgeInfo.java
+++ src/main/java/network/EdgeInfo.java
@@ -1,5 +1,6 @@
 package network;
 
 public class EdgeInfo {
+	String name;
 
 }
--- src/main/java/network/Node.java
+++ src/main/java/network/Node.java
@@ -11,7 +11,35 @@ public class Node {
 		this.edges = edges;
 		this.info = info;
 	}
+
+	public List<Edge> getEdges() {
+		return edges;
+	}
+
+	public void setEdges(List<Edge> edges) {
+		this.edges = edges;
+	}
+
+	public NodeInfo getInfo() {
+		return info;
+	}
+
+	public void setInfo(NodeInfo info) {
+		this.info = info;
+	}
 	
 	
+	public boolean equals (Object o){
+		if(o == null) 
+			return false;
+		if (o instanceof Node){
+			Node n = (Node) o;
+			if (n.info.equals(this.info))
+				return true;
+		}
+		
+		return false;
+		
+	}
 
 }
